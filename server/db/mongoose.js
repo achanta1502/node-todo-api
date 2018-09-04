8cd6fa1770c2c2ea95c1502e7b54bdb2ddcf8e8e
pavan kumar achanta <38113680+achanta1502@users.noreply.github.com>
2018-09-03 22:24:10 -0500

    Updated some ports

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise=global.Promise;
-
+//var app
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
 const {ObjectID}=require('mongodb');
 var app=express();
+
 const port=process.env.PORT || 3000;
 app.use(bodyParser.json())
 app.post('/todos',(req,res)=>{
