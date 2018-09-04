576ea3a83877135f8ab78cb032505e4ae56d64c0
pavan kumar achanta <38113680+achanta1502@users.noreply.github.com>
2018-09-03 20:06:59 -0500

    Add GET/todos id

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 var {mongoose} =require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
-
+const {ObjectID}=require('mongodb');
 var app=express();
 app.use(bodyParser.json())
 app.post('/todos',(req,res)=>{
@@ -23,6 +23,23 @@ app.get('/todos',(req,res)=>{
   },(e)=>{
     res.status(400).send(e);
   })
+});
+app.get('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  if(!ObjectID.isValid(id)){
+    console.log('Id is invalid');
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send(e);
+  });
+
+
 });
 
 app.listen(3000,()=>{
