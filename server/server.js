68bd63db0d627330d7e1c4b32150dee068df6568
pavan kumar achanta <38113680+achanta1502@users.noreply.github.com>
2018-09-08 19:01:59 -0500

    Add private method to access todos

--- server/server.js
+++ server/server.js
@@ -20,9 +20,10 @@ var app=express();
 
 const port=process.env.PORT || 3000;
 app.use(bodyParser.json())
-app.post('/todos',(req,res)=>{
+app.post('/todos',authenticate,(req,res)=>{
   var todo=new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator:req.user._id
   });
   todo.save().then((doc)=>{
     res.send(doc);
@@ -31,20 +32,23 @@ app.post('/todos',(req,res)=>{
   });
 });
 
-app.get('/todos',(req,res)=>{
-  Todo.find().then((todos)=>{
+app.get('/todos',authenticate,(req,res)=>{
+  Todo.find({_creator:req.user._id}).then((todos)=>{
     res.send({todos});
   },(e)=>{
     res.status(400).send(e);
   })
 });
-app.get('/todos/:id',(req,res)=>{
+app.get('/todos/:id',authenticate,(req,res)=>{
   var id=req.params.id;
   if(!ObjectID.isValid(id)){
     console.log('Id is invalid');
     return res.status(404).send();
   }
-  Todo.findById(id).then((todo)=>{
+  Todo.findOne({
+    _id:id,
+  _creator:req.user._id
+}).then((todo)=>{
     if(!todo){
       return res.status(404).send();
     }
@@ -55,12 +59,15 @@ app.get('/todos/:id',(req,res)=>{
 
 
 });
-app.delete('/todos/:id',(req,res)=>{
+app.delete('/todos/:id',authenticate,(req,res)=>{
   var id=req.params.id;
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
   }
-  Todo.findByIdAndRemove(id).then((todo)=>{
+  Todo.findOneAndRemove({
+    _id:id,
+    _creator:req.user._id
+  }).then((todo)=>{
     if(!todo){
       return res.status(404).send();
     }
@@ -69,7 +76,7 @@ app.delete('/todos/:id',(req,res)=>{
     res.status(400).send(e);
   });
 });
-app.patch('/todos/:id',(req,res)=>{
+app.patch('/todos/:id',authenticate,(req,res)=>{
   var id=req.params.id;
   var body = _.pick(req.body,['text','completed']);
   if(!ObjectID.isValid(id)){
@@ -81,7 +88,9 @@ app.patch('/todos/:id',(req,res)=>{
     body.completed=false;
     body.completedAt=null;
   }
-Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo)=>{
+Todo.findOneAndUpdate({_id:id,
+_creator:req.user.id
+},{$set: body},{new: true}).then((todo)=>{
   if(!todo){
     res.status(404).send();
   }
